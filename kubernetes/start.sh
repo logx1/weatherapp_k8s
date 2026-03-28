96980d50d33a18e7fa3dbaa9992a4b206c5324e3
Abdelmajid El-oualy <abdel-ou@c1r7p2.1337.ma>
2026-03-28 11:05:12 +0100

    complie database statefulset ress

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 k3d cluster delete weatherapp-cluster
-k3d cluster create weatherapp-cluster --servers 1 --agents 1 --port 8001:8001@loadbalancer
+k3d cluster create weatherapp-cluster --servers 1 --agents 1 --port 3306:3306@loadbalancer
 
 kubectl create secret generic mysql-secret \
   --from-literal=root-password='secure-root-pw' \
   --from-literal=auth-password='my-secret-pw' \
   --from-literal=secret-key='xco0sr0fh4e52x03g9mv'
+
+
+kubectl create configmap mysql-config --from-file=init.sql=init.sql
